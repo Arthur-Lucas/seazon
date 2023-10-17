03e0402724eb5ea9a351fd652781bcf43e63047f
Arthur <arthurlucas92@orange.fr>
2023-10-17 19:28:15 +0200

    :bug: [T-9] Fix: test var replaced

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,6 +4,6 @@ describe("getThemeFromDate", () => {
   it("should return color from date theme", () => {
     // Date must be of type "MM/dd"
     const result = Theme.getThemeFromDate("06/20", "12/22");
-    expect(typeof randomNumber).toBe("string");
+    expect(typeof result).toBe("string");
   });
 });
