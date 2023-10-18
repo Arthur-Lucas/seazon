dbf8745d5bcd3b800479342b86943fd2631fa409
Arthur <arthurlucas92@orange.fr>
2023-10-18 15:12:53 +0200

    ✨[T-9] Feat: link function with css working

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,6 +4,6 @@ describe("getThemeFromDate", () => {
   it("should return color from date theme", () => {
     // Date must be of type "MM/dd"
     const result = Theme.getThemeFromDate("20/06", "22/12");
-    expect(typeof result).toBe("object");
+    expect(typeof result).toBe("array");
   });
 });
